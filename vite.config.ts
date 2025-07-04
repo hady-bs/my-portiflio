08c100a1389ef0fd0b1ce15ae513d1b2637d4741
hady-bs <aaa88484@gmail.com>
2025-07-04 18:02:35 +0300

    ss

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/my-portfolio/",
+  base: "/my-portfolio/dist",
   plugins: [react(), tailwindcss()],
 });

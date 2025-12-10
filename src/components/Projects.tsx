4c5010005d38b6f174b91c44acaf6920a9bfe211
hady-bs <aaa88484@gmail.com>
2025-12-10 14:28:52 +0300

    hh

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,7 +4,7 @@ import type { Project } from "../types/types";
 const ProjectItem = lazy(() => import("./ProjectItem"));
 export default function Projects({ data }: { data: Project[] }) {
   return (
-    <div className="  bg-gradient-to-t from-primary from-95% to-transparent backdrop-blur-lg flex flex-col gap-10 py-10  ">
+    <div className="  bg-gradient-to-t from-primary from-95% to-primary/50 backdrop-blur-lg flex flex-col gap-10 py-10  ">
       <h2 className="text-4xl font-bold text-center mb-12 bg-gradient-to-r from-aura  bg-clip-text text-transparent">
         Projects
       </h2>
